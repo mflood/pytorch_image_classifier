1455b4aac77cdee7d835f0d2725acc61285fecd4
Matthew Flood <hermannwest@gmail.com>
2019-03-03 08:41:19 -0700

    Add assets

---
+++ log_setup.py
@@ -0,0 +1,15 @@
+"""
+    log_setup.py
+"""
+import logging
+
+def init(loglevel=logging.DEBUG):
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    channel = logging.StreamHandler()
+    channel.setLevel(loglevel)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    channel.setFormatter(formatter)
+    logger.addHandler(channel)
+
+    return logger
